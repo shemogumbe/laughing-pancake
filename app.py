58ff23075e4f171b9268451c132d85489d4e662c
shemogumbe <shemnashon@gmail.com>
2020-12-20 12:58:17 +0300

    add filters

--- app.py
+++ app.py
@@ -16,18 +16,45 @@ db = SQLAlchemy(app)
 from models import FinancialData
 
 
+def _get_all_products():
+    return (
+        db.session.query(FinancialData.product)
+        .distinct(FinancialData.product)
+        .order_by(FinancialData.product)
+        .all()
+    )
+
+
+def _get_all_departments():
+    return (
+        db.session.query(FinancialData.department)
+        .distinct(FinancialData.department)
+        .order_by(FinancialData.department)
+        .all()
+    )
+
+
+def _get_all_countries():
+    return (
+        db.session.query(FinancialData.country)
+        .distinct(FinancialData.country)
+        .order_by(FinancialData.country)
+        .all()
+    )
+
+
 def _get_financial_data():
-    qry = db.session.query(
+    query = db.session.query(
         FinancialData.date,
         func.sum(FinancialData.gross_sales).label("gross_sales"),
         func.sum(FinancialData.sales).label("sales"),
         func.sum(FinancialData.cogs).label("cogs"),
         func.sum(FinancialData.profit).label("profit"),
     )
-    qry = qry.group_by(FinancialData.date).order_by(FinancialData.date)
+    query = query.group_by(FinancialData.date).order_by(FinancialData.date)
 
     gross_sales, sales, cogs, profit = [], [], [], []
-    for _res in qry.all():
+    for _res in query.all():
         data_date = datetime.combine(_res.date, datetime.min.time())
         date_milliseconds = data_date.timestamp() * 1000
         gross_sales.append([date_milliseconds, _res.gross_sales])
@@ -47,10 +74,21 @@ def _get_financial_data():
 def home():
     table = FinancialData.query.order_by(FinancialData.date.desc()).all()
     financial = _get_financial_data()
+
+    products = _get_all_products()
+    departments = _get_all_departments()
+    countries = _get_all_countries()
+
     return render_template(
         "hello.jinja2",
         table=[list(i.to_dict().values()) for i in table],
         financial=financial,
+        all_countries=[x.country for x in countries],
+        selected_countries=[],
+        all_departments=[x.department for x in departments],
+        selected_departments=[],
+        all_products=[x.product for x in products],
+        selected_products=[],
     )
 
 
